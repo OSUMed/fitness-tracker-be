e38cccedb66ba33e6530deade6df9edf666fcf88
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-12-22 00:04:07 -0800

    add basic admin controller with Cross Origin annotation

---
+++ src/main/java/com/srikanth/fitnesstrackerbe/web/AdminController.java
@@ -0,0 +1,31 @@
+package com.srikanth.fitnesstrackerbe.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:5173")
+public class AdminController {
+
+	@GetMapping("/users")
+	public ResponseEntity<String> getAllUsers() {
+		// Simple message for testing
+		String message = "List of all users would be here.!!";
+		
+		// Return the message with an OK (200) status
+		return ResponseEntity.ok(message);
+	}
+	@GetMapping("/private")
+    public ResponseEntity<Map<String, String>> getPrivate() {
+        Map<String, String> response = new HashMap<>();
+        response.put("username", "George");
+
+        return ResponseEntity.ok(response);
+    }
+
+}
