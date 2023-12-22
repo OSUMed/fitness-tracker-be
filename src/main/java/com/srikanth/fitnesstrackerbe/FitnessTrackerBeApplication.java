9bcf2499d236927598db8b3165ccc25ff423292e
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-12-21 22:16:08 -0800

    initial commit

---
+++ src/main/java/com/srikanth/fitnesstrackerbe/FitnessTrackerBeApplication.java
@@ -0,0 +1,13 @@
+package com.srikanth.fitnesstrackerbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessTrackerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessTrackerBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/srikanth/fitnesstrackerbe/FitnessTrackerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srikanth.fitnesstrackerbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessTrackerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
