a652442140b366ce16f568cce10cc280c06ec887
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-12-22 00:04:31 -0800

    add 1st iteration of security config. working HTTP Basic security with front end

---
+++ src/main/java/com/srikanth/fitnesstrackerbe/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.srikanth.fitnesstrackerbe.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173")); // Allowed origins
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Allowed methods
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With")); // Allowed headers
+        configuration.setAllowCredentials(true); // If you need to send credentials
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // Apply to all paths
+        return source;
+    }
+}
---
+++ src/main/java/com/srikanth/fitnesstrackerbe/security/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.srikanth.fitnesstrackerbe.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.security.config.Customizer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
+        http
+            .addFilterBefore(new CorsFilter(corsConfigurationSource), CsrfFilter.class) // Add CorsFilter
+            .csrf(csrf -> csrf.disable()) // Optionally disable CSRF
+            .authorizeHttpRequests((authz) -> authz
+        		.requestMatchers("/private/**").authenticated()
+                .anyRequest().permitAll() // Require authentication for all requests
+            )
+            .httpBasic(Customizer.withDefaults()); // Enable HTTP Basic Authentication with default settings
+
+        return http.build();
+    }
+
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication()
+            .withUser("username")
+            .password(encoder.encode("123"))
+            .roles("USER");
+    }
+}
