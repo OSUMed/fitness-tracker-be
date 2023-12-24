5e8661f2cfe3752a277afa3ba7b2820d22698b61
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-12-23 18:56:36 -0800

    update cookie util to include servlet and responsecookie variations
    ResponseCookie: can be used in the controller following conventions
    Servlet Cookie: can be used in JWT service and others

--- src/main/java/com/srikanth/fitnesstrackerbe/util/CookieUtils.java
+++ src/main/java/com/srikanth/fitnesstrackerbe/util/CookieUtils.java
@@ -1,18 +1,36 @@
 package com.srikanth.fitnesstrackerbe.util;
 
+import org.springframework.http.ResponseCookie;
+
 import jakarta.servlet.http.Cookie;
 
 public class CookieUtils {
 
-	public static Cookie createAccessTokenCookie(String value) {
-		Cookie accessTokenCookie = new Cookie("accessToken", value);
-    	
-		return accessTokenCookie;
-	}
-	
-	public static Cookie createRefreshTokenCookie(String value) {
-		Cookie refreshTokenCookie = new Cookie("refreshToken", value);
-		
-		return refreshTokenCookie;
-	}		
+    // Existing method for creating a refresh token cookie
+    public static ResponseCookie createRefreshTokenCookie(String value, long expiresIn) {
+        return ResponseCookie.from("refreshToken", value)
+                             .path("/")
+                             .httpOnly(true)
+                             .maxAge(expiresIn)
+                             .secure(true) // HTTPS
+                             .build();
+    }
+
+    // New method for clearing a cookie
+    public static ResponseCookie clearCookie(String name) {
+        return ResponseCookie.from(name, "")
+                             .path("/")
+                             .httpOnly(true)
+                             .maxAge(0) // Expire the cookie immediately
+                             .secure(true) // HTTPS
+                             .build();
+    }
+    public static Cookie clearServletCookie(String name) {
+        Cookie cookie = new Cookie(name, "");
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(0); // Expire the cookie immediately
+        cookie.setSecure(true); // HTTPS
+        return cookie;
+    }
 }
