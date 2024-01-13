4780a31a6125d48dc934cff330ae7f245267b737
OSUMed <62990411+OSUMed@users.noreply.github.com>
2024-01-13 03:03:18 -0800

    Add Entity annotation on TodaysWorkout

--- src/main/java/com/srikanth/fitnesstrackerbe/domain/workout/Exercise.java
+++ src/main/java/com/srikanth/fitnesstrackerbe/domain/workout/Exercise.java
@@ -14,7 +14,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 
-import com.srikanth.fitnesstrackerbe.dao.workout.ExerciseSetDTO;
 import com.srikanth.fitnesstrackerbe.domain.User;
 
 @Entity
--- src/main/java/com/srikanth/fitnesstrackerbe/domain/workout/TodaysWorkout.java
+++ src/main/java/com/srikanth/fitnesstrackerbe/domain/workout/TodaysWorkout.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,6 +14,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 
+@Entity
 public class TodaysWorkout {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
