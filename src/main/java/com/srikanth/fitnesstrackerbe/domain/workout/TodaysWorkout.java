c532770688ff3eac9f4b3e6be2ecfbf5a1dc0a6e
OSUMed <62990411+OSUMed@users.noreply.github.com>
2024-01-13 02:51:11 -0800

    add relationship and cascade options between exercise log and todays workout

--- src/main/java/com/srikanth/fitnesstrackerbe/domain/workout/TodaysWorkout.java
+++ src/main/java/com/srikanth/fitnesstrackerbe/domain/workout/TodaysWorkout.java
@@ -9,6 +9,8 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 
 public class TodaysWorkout {
@@ -25,12 +27,24 @@ public class TodaysWorkout {
 	@OneToMany(mappedBy = "workout", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
 	private List<Exercise> exercises;
 
+	@ManyToOne(cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH })
+	@JoinColumn(name = "exercise_log_id")
+	private ExerciseLog exerciseLog;
+
 	public Integer getId() {
 		return id;
 	}
 
 	public void setId(Integer id) {
 		this.id = id;
+	}
+
+	public ExerciseLog getExerciseLog() {
+		return exerciseLog;
+	}
+
+	public void setExerciseLog(ExerciseLog exerciseLog) {
+		this.exerciseLog = exerciseLog;
 	}
 
 	public Integer getUserId() {
@@ -56,5 +70,5 @@ public class TodaysWorkout {
 	public void setExercises(List<Exercise> exercises) {
 		this.exercises = exercises;
 	}
-	
+
 }
