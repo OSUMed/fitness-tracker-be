dbf477d3674a57acc43e57f9071be9bd30ed4a52
OSUMed <62990411+OSUMed@users.noreply.github.com>
2024-01-17 20:18:17 -0800

    add working deleteExerciseFromTodaysWorkout to delete exercise from Todays Workout
    1. also add exception methods: NoWorkoutFoundException & ExerciseNotFoundException
    2. remove given id from today's exercise list
    3. Removes directly with removeIf()

--- src/main/java/com/srikanth/fitnesstrackerbe/service/workout/TodaysWorkoutTableService.java
+++ src/main/java/com/srikanth/fitnesstrackerbe/service/workout/TodaysWorkoutTableService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.srikanth.fitnesstrackerbe.domain.User;
@@ -15,6 +17,7 @@ import com.srikanth.fitnesstrackerbe.repository.workout.TodaysWorkoutRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.ArrayList;
 import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 import com.srikanth.fitnesstrackerbe.dao.workout.*;
@@ -31,17 +34,13 @@ public class TodaysWorkoutTableService {
 
 	public TodaysWorkout processTodaysWorkoutData(Map<String, Object> fullExerciseData) {
 		ExerciseDTO exerciseDTO = exerciseService.convertDataToExerciseDTO(fullExerciseData);
-		System.out.println("final exerciseDTO: " + exerciseDTO + " exerciseDTO's userId: "
-				+ exerciseDTO.getUserId());
+		System.out.println("final exerciseDTO: " + exerciseDTO + " exerciseDTO's userId: " + exerciseDTO.getUserId());
 		Exercise exercise = exerciseService.convertExerciseDTOToExercise(exerciseDTO);
 		System.out.println("exercise domain: " + exercise);
 		User user = userRepository.findById(exerciseDTO.getUserId())
 				.orElseThrow(() -> new RuntimeException("User not found"));
 		java.util.Date todayDate = new java.util.Date();
 		java.sql.Date sqlDate = new java.sql.Date(todayDate.getTime());
-
-
-
 
 		Optional<TodaysWorkout> existingWorkout = todaysWorkoutRepository.findByUserIdAndDate(user.getId(), sqlDate);
 
@@ -54,7 +53,7 @@ public class TodaysWorkoutTableService {
 			todaysWorkout.setDate(sqlDate);
 			todaysWorkout.setExercises(new ArrayList<Exercise>());
 		}
-		
+
 		// Add the exercise to today's workout
 		exercise.setWorkout(todaysWorkout);
 		todaysWorkout.getExercises().add(exercise);
@@ -67,12 +66,11 @@ public class TodaysWorkoutTableService {
 	}
 
 	public TodaysWorkout addExerciseToTodayWorkout(Map<String, Object> workoutData) {
-
 		return new TodaysWorkout();
 	}
 
 	public TodaysWorkoutDTO returnTodaysWorkoutData(TodaysWorkout workoutData) {
-		
+
 		// Get the TodaysWorkout from Repo
 		java.sql.Date utilDate = workoutData.getDate();
 		Integer userId = workoutData.getUserId();
@@ -80,12 +78,12 @@ public class TodaysWorkoutTableService {
 		Optional<TodaysWorkout> databaseTodaysWorkout = todaysWorkoutRepository.findByUserIdAndDate(userId, utilDate);
 		TodaysWorkoutDTO todaysWorkoutDTO = null;
 		System.out.println("postREPO Todays Workout: " + databaseTodaysWorkout);
-		
+
 		// Change it to DTO
 		if (databaseTodaysWorkout.isPresent()) {
 			todaysWorkoutDTO = exerciseService.convertDomainToDTO(databaseTodaysWorkout.get());
 		}
-		
+
 		System.out.println("Finished TodaysWorkoutDTO: " + databaseTodaysWorkout);
 		// Return DTO
 		return todaysWorkoutDTO;
@@ -93,8 +91,43 @@ public class TodaysWorkoutTableService {
 
 	public Optional<TodaysWorkout> findTodaysWorkoutByUser(Integer userId) {
 		return todaysWorkoutRepository.findByUserId(userId);
-	
+
 	}
 
-	
+	public TodaysWorkoutDTO deleteExerciseFromTodaysWorkout(Integer userId, Integer exerciseId) {
+		Long exerciseIdLong = Long.valueOf(exerciseId);
+		Optional<TodaysWorkout> todaysWorkoutOpt = findTodaysWorkoutByUser(userId);
+
+		if (todaysWorkoutOpt.isPresent()) {
+			TodaysWorkout todaysWorkout = todaysWorkoutOpt.get();
+			boolean removed = todaysWorkout.getExercises()
+					.removeIf(exercise -> exercise.getId().equals(exerciseIdLong));
+
+			if (!removed) {
+				throw new ExerciseNotFoundException("Exercise ID " + exerciseId + " not found");
+			}
+
+			TodaysWorkout savedTodaysWorkout = todaysWorkoutRepository.save(todaysWorkout);
+			return returnTodaysWorkoutData(savedTodaysWorkout);
+		} else {
+			throw new NoWorkoutFoundException("No workout found for user ID " + userId);
+		}
+	}
+
+	public class NoWorkoutFoundException extends RuntimeException {
+		private static final long serialVersionUID = 1L;
+
+		public NoWorkoutFoundException(String message) {
+			super(message);
+		}
+	}
+
+	public class ExerciseNotFoundException extends RuntimeException {
+		private static final long serialVersionUID = 1L;
+
+		public ExerciseNotFoundException(String message) {
+			super(message);
+		}
+	}
+
 }
