acdb2f75d00074dfb541ab619d42242f7b36f4b6
OSUMed <62990411+OSUMed@users.noreply.github.com>
2024-01-19 09:08:13 -0800

    add more configs: max age to several hours, allow the headers for react to catch

--- src/main/java/com/srikanth/fitnesstrackerbe/config/CorsConfig.java
+++ src/main/java/com/srikanth/fitnesstrackerbe/config/CorsConfig.java
@@ -17,8 +17,10 @@ public class CorsConfig {
         configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5173")); // React app's origin
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
-        configuration.setAllowCredentials(true); // Important for cookies, authorization headers
-
+        configuration.setExposedHeaders(Arrays.asList("Authorization-Refresh", "Refresh-Token-Expired"));
+        configuration.setAllowCredentials(true); 
+        configuration.setMaxAge(3600L); 
+        
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
