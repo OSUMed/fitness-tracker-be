afab0042d551bee2cffe99f27d2da7c2a2c6231f
OSUMed <62990411+OSUMed@users.noreply.github.com>
2024-01-21 03:12:51 -0800

    Add working POST endpoint and TEST endpoint, add debug statements

--- src/main/java/com/srikanth/fitnesstrackerbe/web/WeeklyPlanController.java
+++ src/main/java/com/srikanth/fitnesstrackerbe/web/WeeklyPlanController.java
@@ -1,13 +1,17 @@
 package com.srikanth.fitnesstrackerbe.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.srikanth.fitnesstrackerbe.dao.workout.TodaysWorkoutDTO;
 import com.srikanth.fitnesstrackerbe.domain.weekplan.DayPlan;
 import com.srikanth.fitnesstrackerbe.domain.weekplan.WeeklyPlan;
 import com.srikanth.fitnesstrackerbe.service.weekplan.WeeklyPlanService;
@@ -15,6 +19,8 @@ import com.srikanth.fitnesstrackerbe.service.weekplan.WeeklyPlanService;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/weekplan")
 public class WeeklyPlanController {
 
 	private final WeeklyPlanService weeklyPlanService;
@@ -24,28 +30,40 @@ public class WeeklyPlanController {
 		this.weeklyPlanService = weeklyPlanService;
 	}
 
-	@GetMapping("/weeklyplan")
-	public WeeklyPlan getWeeklyPlan() {
-		return weeklyPlanService.returnWeekPlan(null);
+	@GetMapping("/test")
+	public ResponseEntity<String> testWeeklyPlan() {
+		System.out.println("getWeeklyPlan: ");
+		return ResponseEntity.ok("Weekly Plan Test endpoint from eclipse!");
+	}
+	@GetMapping("/")
+	public ResponseEntity<WeeklyPlan> getWeeklyPlan() {
+		System.out.println("getWeeklyPlan: ");
+		return ResponseEntity.ok(weeklyPlanService.returnWeekPlan());
 	}
 
-	@PostMapping("/weeklyplan")
-	public WeeklyPlan createWeeklyPlan(@RequestBody List<DayPlan> dayPlans) {
+	@PostMapping("/")
+	public ResponseEntity<WeeklyPlan> createWeeklyPlan(@RequestBody List<DayPlan> dayPlans) {
+		System.out.println("createWeeklyPlan: " + dayPlans);
 		WeeklyPlan weeklyPlan = weeklyPlanService.processIncomingDataToDomain(dayPlans);
+		System.out.println("createWeeklyPlan weeklyPlan: " + weeklyPlan);
 		WeeklyPlan savedWeekPlan = weeklyPlanService.saveWeeklyPlan(weeklyPlan);
-		return weeklyPlanService.returnWeekPlan(savedWeekPlan.getId());
+		System.out.println("createWeeklyPlan weeklyPlan: " + savedWeekPlan);
+		return ResponseEntity.ok(weeklyPlanService.returnWeekPlan());
 	}
 
-	@PutMapping("/weeklyplan")
-	public WeeklyPlan putWeeklyPlan(@RequestBody List<DayPlan> dayPlans) {
+	@PutMapping("/")
+	public ResponseEntity<WeeklyPlan> putWeeklyPlan(@RequestBody List<DayPlan> dayPlans) {
+		System.out.println("putWeeklyPlan: " + dayPlans);
 		WeeklyPlan weeklyPlan = weeklyPlanService.processIncomingDataToDomain(dayPlans);
 		weeklyPlanService.updateWeeklyPlan(weeklyPlan);
-		return weeklyPlanService.returnWeekPlan(null);
+		return ResponseEntity.ok(weeklyPlanService.returnWeekPlan());
 	}
 
-	@DeleteMapping("/weeklyplan")
-	public WeeklyPlan deleteWeeklyPlan() {
-		weeklyPlanService.deleteWeeklyPlan();
-		return new WeeklyPlan();
+	@DeleteMapping("/")
+	public ResponseEntity<WeeklyPlan> deleteWeeklyPlan() {
+		System.out.println("deleteWeeklyPlan: ");
+		WeeklyPlan thisWeekPlan = weeklyPlanService.deleteWeeklyPlan();
+		System.out.println("deleteWeeklyPlan returned week: " + thisWeekPlan);
+		return ResponseEntity.ok(thisWeekPlan);
 	}
 }
