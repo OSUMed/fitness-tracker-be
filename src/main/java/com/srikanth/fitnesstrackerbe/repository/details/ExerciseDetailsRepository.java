968b2b6b289372cd2c765e3077433775fb5dcb6c
OSUMed <62990411+OSUMed@users.noreply.github.com>
2024-01-22 01:16:58 -0800

    add find by type query for filtering

--- src/main/java/com/srikanth/fitnesstrackerbe/repository/details/ExerciseDetailsRepository.java
+++ src/main/java/com/srikanth/fitnesstrackerbe/repository/details/ExerciseDetailsRepository.java
@@ -2,9 +2,14 @@ package com.srikanth.fitnesstrackerbe.repository.details;
 
 
 import com.srikanth.fitnesstrackerbe.domain.details.ExerciseDetails;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ExerciseDetailsRepository extends JpaRepository<ExerciseDetails, Long> {
+
+	List<ExerciseDetails> findByType(String exerciseDetailsType);
 }
