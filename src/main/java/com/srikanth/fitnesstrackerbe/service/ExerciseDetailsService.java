3feaa3b90afe77e10f285ae9745e1326698962ab
OSUMed <62990411+OSUMed@users.noreply.github.com>
2024-01-22 01:04:12 -0800

    add Exercise Details Service skeleton

---
+++ src/main/java/com/srikanth/fitnesstrackerbe/service/ExerciseDetailsService.java
@@ -0,0 +1,37 @@
+package com.srikanth.fitnesstrackerbe.service;
+
+import java.util.List;
+
+import com.srikanth.fitnesstrackerbe.domain.details.ExerciseDetails;
+
+public class ExerciseDetailsService {
+
+	public List<ExerciseDetails> returnAllDetails() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ExerciseDetails postExerciseDetail(List<ExerciseDetails> exerciseDetail) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public ExerciseDetails putExerciseDetail(ExerciseDetails exerciseDetail) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	public List<ExerciseDetails> filterExerciseDetail(String exerciseDetailsType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public ExerciseDetails deleteExerciseDetail(String exerciseDetailId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
